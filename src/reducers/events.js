01cf5e3e37acb9d65e0416a2e429f3ec38799303
Daniel Ionita <danielionita88@yahoo.com>
2020-02-03 13:12:27 -0600

    first commit

---
+++ src/reducers/events.js
@@ -0,0 +1,10 @@
+const events = (state={}, action)=>{
+    switch(action.type){
+        case 'NEW':
+            return state
+        default:
+            return state
+    }
+}
+
+export default events;
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux'
+import user from './user'
+import events from './events'
+
+
+export default combineReducers({
+    user,
+    events
+})
+
