71a3dfc594a25f30647dd6094f7f4b17793479c4
Daniel Ionita <danielionita88@yahoo.com>
2020-03-14 14:39:56 -0500

    render the events users name on home page

--- src/components/ShowFriendsEvents.js
+++ src/components/ShowFriendsEvents.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {getFriends} from '../actions/network'
-import {Segment} from 'semantic-ui-react'
+import {Segment, Button} from 'semantic-ui-react'
 
 class ShowFriendsEvents extends React.Component{
 
     renderFriendsEvents=()=>{
-        let events = this.props.friends.map(friend =>friend.events).flat()
-        console.log(events)
-        return events.map(event => <Segment key={event.id}>
-            
+        let events = this.props.friends.map(friend =>friend.events).flat().reverse()
+        return events.map(event => {
+        let user = this.props.friends.find(user => user.id === event.user_id)
+        return<Segment key={event.id}>
+            <h3>{user.first_name} {user.last_name} created this event:</h3>
             <h4>{event.name}</h4>
             <p>Location: {event.location}</p>
             <p>Date/Hour: {event.date} at {event.time}</p>
             <p>Details: {event.details}</p>
-        </Segment>)
+            <Button>Join</Button> <Button> Comment </Button>
+        </Segment>})
     }
 
     render(){
